c4b9be86e691e7729f5a39ec86bb36712dcf3829
youngjin <yoho555@icloud.com>
2022-01-21 17:19:20 +0900

    feat: producer, consumer messaging

--- src/main/kotlin/com/example/kafka/Aggregator/EventConsumerImpl.kt
+++ src/main/kotlin/com/example/kafka/Aggregator/EventConsumerImpl.kt
@@ -1,8 +1,7 @@
 package com.example.kafka.Aggregator
 
-import com.example.kafka.usecase.event.*
+import com.example.kafka.usecase.event.* // ktlint-disable no-wildcard-imports
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.core.json.JsonReadFeature
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
@@ -12,9 +11,6 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.slf4j.LoggerFactory
-import org.springframework.messaging.Message
-import org.springframework.util.StringUtils
-import reactor.core.publisher.Sinks
 import reactor.kafka.receiver.KafkaReceiver
 import java.lang.RuntimeException
 import java.time.LocalDateTime
@@ -29,19 +25,6 @@ class EventConsumerImpl(
     private val log = LoggerFactory.getLogger(EventPublisherImpl::class.java)
 
     companion object {
-        //        private val eventObjectMapper = ObjectMapper()
-//            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-//            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-//            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-//            .registerModule(KotlinModule())
-//            .registerModule(
-//                JavaTimeModule().addSerializer(
-//                    LocalDateTime::class.java,
-//                    LocalDateTimeSerializer(
-//                        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
-//                    )
-//                )
-//            )
         private val eventObjectMapper = ObjectMapper()
             .apply {
                 this.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
@@ -53,56 +36,80 @@ class EventConsumerImpl(
                 this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
                 this.registerModule(KotlinModule())
-                this.registerModule(JavaTimeModule().apply {
-                    val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-                    addDeserializer(
-                        LocalDateTime::class.java,
-                        LocalDateTimeDeserializer(dateFormat)
-                    )
-                    addSerializer(
-                        LocalDateTime::class.java,
-                        LocalDateTimeSerializer(dateFormat)
-                    )
-                })
+                this.registerModule(
+                    JavaTimeModule().apply {
+                        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                        addDeserializer(
+                            LocalDateTime::class.java,
+                            LocalDateTimeDeserializer(dateFormat)
+                        )
+                        addSerializer(
+                            LocalDateTime::class.java,
+                            LocalDateTimeSerializer(dateFormat)
+                        )
+                    }
+                )
             }
     }
-
 
     override suspend fun deleteEventConsumer() {
         // receive message
         val kafkaFlux = kafkaReceiver.receive()
         kafkaFlux.subscribe {
-            println()
-            println()
-            println()
             println("========================Start==================================")
-            val strIndex = it.value().indexOfFirst { c -> c == '{' }
-            val json = it.value().substring(strIndex)
-            val ba = eventObjectMapper.readValue(json, BaseEvent::class.java)
-            val account = when (ba.type) {
-                BusinessAccountEventType.BA_CREATE -> eventObjectMapper.readValue(
-                    json,
-                    CreateBusinessAccountEvent::class.java
-                )
-                BusinessAccountEventType.BA_UPDATE -> eventObjectMapper.readValue(
-                    json,
-                    UpdateBusinessAccountEvent::class.java
-                )
-                BusinessAccountEventType.BA_DELETE -> eventObjectMapper.readValue(
-                    json,
-                    DeleteBusinessAccountEvent::class.java
-                )
-                BusinessAccountEventType.BA_UNDELETE -> eventObjectMapper.readValue(
-                    json,
-                    UndeleteBusinessAccountEvent::class.java
-                )
-                else -> RuntimeException("type error")
+            if (it.value().contains(BusinessAccountEventType.BA_UPDATE) || it.value()
+                .contains(BusinessAccountEventType.BA_DELETE) || it.value()
+                    .contains(BusinessAccountEventType.BA_UNDELETE)
+            ) {
+                println("========================Check finished==================================")
+                val strIndex = it.value().indexOfFirst { c -> c == '{' }
+                val json = it.value().substring(strIndex)
+                val ba = eventObjectMapper.readValue(json, BaseEvent::class.java)
+                var account: Any
+                when (ba.type) {
+                    BusinessAccountEventType.BA_UPDATE -> {
+
+                        account = eventObjectMapper.readValue(
+                            json,
+                            UpdateBusinessAccountEvent::class.java
+                        )
+                        println("BA Update!!")
+                        if (account.afterUpdate.status == "ACTIVE") {
+                            println("store: inActive -> Active \n Product ALL Inactive")
+                        } else if (account.afterUpdate.status == "INACTIVE") {
+                            println("store: Active -> inActive \n Product ALL Inactive")
+                        } else {
+                            println("None!")
+                        }
+                    }
+                    BusinessAccountEventType.BA_DELETE -> {
+
+                        account = eventObjectMapper.readValue(
+                            json,
+                            DeleteBusinessAccountEvent::class.java
+                        )
+                        println("BA Delete!!")
+                        println("Store, Product Delete !!")
+                    }
+                    BusinessAccountEventType.BA_UNDELETE -> {
+                        account = eventObjectMapper.readValue(
+                            json,
+                            UndeleteBusinessAccountEvent::class.java
+                        )
+                        println("BA UnDelete!!")
+                        if (account.businessAccountDto.status == "ACTIVE") {
+                            println("store: Active because Active Status of BA")
+                        } else if (account.businessAccountDto.status == "INACTIVE") {
+                            println("store 변화 없음! because InActive Status of BA")
+                        }
+                    }
+                    else -> RuntimeException("type error")
+                }
             }
-            println(account::class.java)
             val offset = it.receiverOffset()
             println(
                 "Received message: topic-partition=${offset.topicPartition()}, offset=${offset.offset()} " +
-                        " key=${it.key()}, value=${it.value()}"
+                    " key=${it.key()}, value=${it.value()}"
             )
             offset.acknowledge()
         }
